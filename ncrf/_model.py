9e2f37644ce5cdccd4d74c252b3158d44c7ef209
AD\proloy <proloy@umd.edu>
2019-10-10 11:24:28 -0400

    FEATURE add post_normalization

--- ncrf/_model.py
+++ ncrf/_model.py
@@ -243,6 +243,7 @@ class REG_Data:
         self.nlevel = nlevel
         self.s_baseline = baseline
         self.s_scaling = scaling
+        self.s_normalization = []
         self.meg = []
         self.covariates = []
         self.tstep = None
@@ -346,6 +347,7 @@ class REG_Data:
             # Mind the normalization
             i += l
         self._n_predictor_variables = len(covariates)
+        self.s_normalization.append([linalg.norm(x, 2) for x in covariates])
 
         x = np.concatenate(covariates, axis=1).astype(np.float64)
         self.covariates.append(x)
@@ -404,6 +406,19 @@ class REG_Data:
 
         return obj
 
+    def post_normalization(self):
+        n_vars = sum(len(dim) if dim else 1 for dim in self._stim_dims)
+        if n_vars > 1:
+            start = 0
+            stim_lens = [len(dim) if dim else 1 for dim in self._stim_dims]
+            basis_lengths = [basis.shape[1] for basis in self.basis]
+            basis_lengths = np.repeat(np.asanyarray(basis_lengths), stim_lens)
+            s_normalization = np.asanyarray(self.s_normalization).mean(axis=0)
+            for basis_length, norm in zip(basis_lengths, s_normalization):
+                for covariates in self.covariates:
+                    covariates[:, start:start+basis_length] /= norm
+                start += basis_length
+
 
 class ncRF:
     """The object-based API for cortical TRF localization
@@ -461,6 +476,7 @@ class ncRF:
     _stim_names = None
     _stim_baseline = None
     _stim_scaling = None
+    _stim_normalization = None
     _basis = None
     tstart = None
     tstep = None
@@ -507,7 +523,8 @@ class ncRF:
 
     _PICKLE_ATTRS = ('_basis', '_cv_results', 'mu',  '_name', '_stim_is_single', '_stim_dims', '_stim_names',
                      'noise_covariance', 'n_iter', 'n_iterc', 'n_iterf', 'lead_field',
-                     '_stim_baseline', '_stim_scaling', 'lead_field_scaling', 'residual', 'source', 'space', 'theta', 'tstart', 'tstep', 'tstop')
+                     '_stim_baseline', '_stim_scaling', 'lead_field_scaling', 'residual', 'source', 'space', 'theta',
+                     'tstart', 'tstep', 'tstop', '_stim_normalization')
 
     def __getstate__(self):
         return {k: getattr(self, k) for k in self._PICKLE_ATTRS}
@@ -838,6 +855,7 @@ class ncRF:
         self._stim_names = data._stim_names
         self._stim_baseline = data.s_baseline
         self._stim_scaling = data.s_scaling
+        self._stim_normalization = data.s_normalization
         self._basis = data.basis
         self.tstart = data.tstart
         self.tstep = data.tstep
